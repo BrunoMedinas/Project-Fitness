16ddd2a9ff5c549824b73f4b8ea7bc1938c2492f
BrunoCod <brunocodfrila@gmail.com>
2023-05-14 17:52:22 -0300

    Frist Code on androidStudio 2

---
+++ app/src/main/java/com/example/fitnesspoy/model/Calc.kt
@@ -0,0 +1,14 @@
+package com.example.fitnesspoy.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity
+data class Calc(
+    @PrimaryKey(autoGenerate = true) val id : Int,
+    @ColumnInfo(name = "type") val type : String,
+    @ColumnInfo(name = "res") val res : Double,
+    @ColumnInfo(name = "create_date") val createDate: Date
+)
